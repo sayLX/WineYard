49bf8f30fb46f30c0eef55d146835ee65a592508
LiuXin <lausinlx@foxmail.com>
2020-11-25 10:42:24 +0800

    my first app

---
+++ myapp/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <!-- <router-link to="/login">login</router-link> | -->
+    <!-- <router-link to="/about">About</router-link>|
+    <router-link to="/">Home</router-link> -->
+    <router-view />
+  </div>  
+</template>
+<style scoped lang="scss">
+    #app{
+      min-width: 1080px;
+
+    }
+</style>
+
---
+++ myapp/src/views/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="index">
+      <!-- <img src="~@/assets/img/登录bg.jpg" alt=""> -->
+      <div id="login">
+          <input type="text">
+          <a-button type="primary">
+      Primary
+    </a-button>
+      </div>
+  </div>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue';
+export default {
+  name: "login",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+#index{
+    min-width: 1920px;
+    height: 1080px;
+    position: relative;
+
+    #login{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    background-image: url('~@/assets/img/登录bg.jpg');
+    background-size:cover;
+}
+
+</style>
