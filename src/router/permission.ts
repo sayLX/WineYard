485540fda983cfa5c06d6d91d31a558635436e05
xieyunzhou <1606810498@qq.com>
2020-12-28 14:37:03 +0800

    页面刷新后保留vuex状态

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -5,18 +5,17 @@ import store from "../store/index";
 router.beforeEach((to, from, next) => {
   if (to.matched.some(res => res.meta.isLogin)) {
     //判断是否需要登录
-    store.getters['isLogin'].then((res) => {
-      if (res) {
-        next();
-      } else {
-        next({
-          path: "/login",
-          query: {
-            redirect: to.fullPath
-          }
-        });
-      }
-    })
+    console.log(store.getters['isLogin'])
+    if (store.getters['isLogin']) {
+      next();
+    } else {
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    }
   } else {
     next();
   }
--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports  = {
         host: 'localhost',  // 匹配本机IP地址(默认是0.0.0.0)
         port: 8000, // 开发服务器运行端口号
         // proxy: null,
-        proxy:{
+        proxy: {
             '/api':{   // 路径中有 /api 的请求都会走这个代理 , 可以自己定义一个,下面移除即可
                 target:'http://192.168.1.19:9990/',    // 目标代理接口地址,实际跨域要访问的接口,这个地址会替换掉 axios.defaults.baseURL
                 secure:false,
